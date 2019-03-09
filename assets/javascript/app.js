01635e0c218eccf0bd35289a72b8c580d8a273b5
bwillstorey <bwillstorey@gmail.com>
2019-03-09 12:42:58 -0600

    adding cards to page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,7 +24,12 @@ $.ajax({
     for (var i = 0; i < 5; i++) {
         var eventName = response._embedded.events[i].name;
         console.log(eventName);
-        $("#events-div").append(eventName);
+
+        var makeCard = $("<div class='card' style='width: 18rem;'><img src='...' class='card-img-top' alt='...'><div class='card-body'><h5 class='card-title'>Card title</h5><p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p><a href='#' class='btn btn-primary'>Go somewhere</a></div></div>")
+        $("#events.div").append(makeCard);
+
+
+        // $("#events-div").append(eventName);
     }
 
 
