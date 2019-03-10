b53a8d36a2e129b8bda51a08f3febb0a0d07692c
Christian Chavez <christianchavez@Christians-MacBook-Pro.local>
2019-03-10 08:33:10 -0500

    added google places api to bring in bars, gets data but does not show on page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 //Place Search
 //Find Place requests
 //link https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters
@@ -14,14 +15,51 @@ var proxyURL = "https://cors-anywhere.herokuapp.com/"
     $.ajax({
         url: proxyURL + queryURL,
         method: "GET"
+=======
+//Need to figure out how to pull ratings (rating), address (vicinity), and pricing (price_level)
+//can you do multiple <p> tags or assign multiple <p> tags for each response data 
+
+
+//on click functon to bring in search when user clicks "search"
+$("#search-button").on("click", ".btn", function() {
+    
+    //query url to request data on bars within 1500 (1 mile) meter radius of nashville
+    var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.1626638,-86.7816016&radius=1500&type=bar&key=AIzaSyBRfIy9kF3qLTHzNFZIykbRJz3CwKJYk84";
+
+    //Ajax request
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+>>>>>>> Stashed changes
     })
 
     .done(function(response) {
         console.log(response);
 
+<<<<<<< Updated upstream
         //var results = response.data
         
         //for (var i = 0; i < results.length; i++) {
 
         
     })
+=======
+ /*       var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+
+            //create a div to hold the response data
+            var barDiv = $("<div>");
+            //create a new <p> tag to add the name
+            var p = $("<p>");
+            p.text(results[i].name);
+            var p = $("<p>").text("Bar Name: " + results[i].name);
+
+            barDiv.append(p);
+
+            $("#bars-div").prepend(barDiv);
+        } */
+    })
+
+})
+>>>>>>> Stashed changes
