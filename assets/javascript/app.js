b272d3f0729d8ca73c1e2a7c972e020ccafe28fe
Christian Chavez <christianchavez@Christians-MacBook-Pro.local>
2019-03-12 20:23:23 -0500

    added js to push results to the dom

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,32 @@
+$(document).ready(function() {
 
-//on click functon to bring in search when user clicks "search"
-$("#search-button").on("click", ".btn", function() {
-    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=bars&location=nashville&limit=6";
+	var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=bars&location=nashville&limit=6";
+$.ajax({
+	 url: myurl,
+	 headers: {
+		'Authorization':'Bearer lxCQuPmOMELdb3m7ZmW59X9CTUOg7ylEV_iJhlKpsuAoFdk8jkqVGY-M0YxAJVeLTxbT2my_T_Wp0byJJsvVdhxNo2TFJH1-DE6cZXAI3iPqTf4jdkMd3q38G1KEXHYx',
+},
+	 method: 'GET',
+	 dataType: 'json',
+	 success: function(data){
+			 console.log(data);
+	 }
+})
+	.done(function(data) {
+			
+			var results = data.businesses;
 
-  $.ajax({
-     url: myurl,
-     headers: {
-      'Authorization':'Bearer lxCQuPmOMELdb3m7ZmW59X9CTUOg7ylEV_iJhlKpsuAoFdk8jkqVGY-M0YxAJVeLTxbT2my_T_Wp0byJJsvVdhxNo2TFJH1-DE6cZXAI3iPqTf4jdkMd3q38G1KEXHYx',
-  },
-     method: 'GET',
-     dataType: 'json',
-      success: function(data){
-          console.log(data);
-      }
+			for (var i = 0; i < results.length; i++) {
+					
+			var barDiv = $("<div>");
+			var p = $("<p>");
+			p.text(results[i].name);
+			var p = $("<p>").text("Name: " + results[i].name);
+			barDiv.append(p); 			
+			$("#results").prepend(barDiv);
 
-  })
-//   .done(function(data) {
-//       console.log(data);
-
-//       var results = data
-
-//       for (var i = 0; i < data.length; i++) {
-//           var barDiv = $("<div>");
-//           var p = $("<p>");
-//           p.text(data[i].name);
-//           var p = $("<p>").text("Name: " + data[i].name);
-
-//           barDiv.append(p);
-//           $("bars-div").prepend(barDiv);
-//       }
-//   })
-
-
+			}
+	
+	});
 
 })
