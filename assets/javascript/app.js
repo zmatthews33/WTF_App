53aa1294c51612503e220a27cf65139ba279bea0
Cameron Forrest <44378657+CameronForrest@users.noreply.github.com>
2019-03-13 20:00:12 -0500

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,31 @@
-// Java———————————————
-
 // placeholder for events api
 $(document).ready(function () {
-
     $(".results-div").hide();
 });
-
 $("#search-button").on("click", function() {
-
+    
+      
     $(".results-div").show();
+    $(".card").remove();
+    
     // clear cards from previous searches
     for (var i = 0; i < 5; i++) {
         $("#band-div").remove();
     }
-
 // select text of the chose date input field
 var dateChosen = $(".date-text").text();
-
 // convert the date format to a readable format for the ticketmaster API
 console.log(dateChosen);
 JSON.stringify(dateChosen);
 var dateFormat = "ddd, MMM DD";
 var convertedDate = moment(dateChosen, dateFormat);
-
 // set the start date time
 var formattedDate = convertedDate.format("YYYY-MM-DDTHH:mm:ssZ");
 console.log(formattedDate);
-
 // set the end date time
 var m = moment(formattedDate, "YYYY-MM-DDTHH:mm:ssZ");
 var endFormattedDate = m.add(23, "hours").format("YYYY-MM-DDTHH:mm:ssZ");
 console.log(endFormattedDate);
-
 // set our query url with the required date search parameters
 var queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=5&apikey=KmtIKgXV9VzRbLsyDRIUc3ntCemOSLIA&startDateTime=" + formattedDate + "&endDateTime=" + endFormattedDate + "&city=Nashville";
 var proxyURL = "https://cors-anywhere.herokuapp.com/"
@@ -39,23 +33,11 @@ $.ajax({
     url: proxyURL + queryUrl,
     type:"GET"
 }).then(function(response) {
-
     console.log(queryUrl);
     console.log(response);
-
     // loop through the 5 result object arrays
     for (var i = 0; i < 5; i++) {
-        // var eventName = response._embedded.events[i].name;
-        // console.log(eventName);
-        // var eventDate = response._embedded.events[i].dates.start.localDate;
-        // console.log(eventDate);
-
-        // // create a new card with the event title and event date
-        // var makeCard = $("<div id='band-div' class='card' style='width: 18rem;'><div class='card-body'>").append("<h5>").text(eventName + ":::  :::" + eventDate)
         
-        // // manipulate the DOM by adding a the new card
-        // $("#events-div").append(makeCard);
-
           var eventTitle = response._embedded.events[i].name;
           var dateText = response._embedded.events[i].dates.start.localDate;
           var image = response._embedded.events[i].images[0].url;
@@ -87,10 +69,6 @@ $.ajax({
           $("#event-dump").append(newCards);
           }
     })
-
-
-
-
   //restaraunt api call to yelp
   var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&location=nashville&limit=5";
   $.ajax({
@@ -151,7 +129,6 @@ $.ajax({
           }
           
   })
-
   //restaraunt api call to yelp
   var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=bars&location=nashville&limit=5";
   $.ajax({
@@ -210,8 +187,6 @@ $.ajax({
           $("#bar-dump").append(newCards);
           }
           
+          
   });
-
 });
-
-// -------------------------------
